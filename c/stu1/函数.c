7a0f3b6d59227583a28ea930c9edc939b9b60128
liumintao2003 <3537693856@qq.com>
2025-08-10 17:56:37 +0800

    提交代码

---
+++ c/stu1/函数.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+
+
+// int Add(int x,int y){
+//     return x+y;
+// }
+
+
+
+
+// int main(){
+
+//     int n1 =0;
+//     int n2 =0;
+//     printf("请输入整数n1\n");
+//     scanf("%d",&n1);
+//     printf("请输入整数n2\n");
+//     scanf("%d",&n2);
+//     int sum = Add(n1,n2);
+//     printf("和为%d",sum);
+
+//     return 0;
+// }
---
+++ c/stu1/循环.c
@@ -0,0 +1,27 @@
+// #include <stdio.h>
+
+// int main(){
+
+
+
+
+//     int line =0;
+//     printf("学习");
+
+
+//     while (line<20000){ 
+//         line++;
+//         printf("写代码,代码行数%d\n",line);
+//     }
+//     if(line==20000){
+//         printf("出师\n");
+//     }
+//     else{
+//         printf("还不行\n");
+//     }
+
+
+
+
+//     return 0;
+// }
