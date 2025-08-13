39aec405650c6b4aae1e17f89aa16616c109902b
liumintao2003 <3537693856@qq.com>
2025-08-13 20:05:38 +0800

    vault backup: 2025-08-13 20:05:38

--- c/stu1/结构体.c
+++ c/stu1/结构体.c
@@ -1,31 +1,34 @@
-#include<stdio.h>
+// #include<stdio.h>
 
 
-struct Stu{
-    char name[20];
-    int age;
-    char sex[10];
-    char tele[12];
-};
+// struct Stu{
+//     char name[20];
+//     int age;
+//     char sex[10];
+//     char tele[12];
+// };
 
-void prt(struct Stu *s){
+// void prt(struct Stu *s){
 
-    printf("%s %d %s %s\n",(*s).name,(*s).age,(*s).sex,(*s).tele);
-    printf("%s %d %s %s\n",s->name,s->age,s->sex,s->tele);
+//     printf("%s %d %s %s\n",(*s).name,(*s).age,(*s).sex,(*s).tele);
+//     //->
+//     //结构体指针变量->成员名
 
-}
+//     printf("%s %d %s %s\n",s->name,s->age,s->sex,s->tele);
 
-
-int main(){
-
-    struct Stu s ={"zhangsan",12,"man","13032866683"};
-
-    printf("%s %d %s %s\n",s.name,s.age,s.sex,s.tele);
-
-    prt(&s);
+// }
 
 
+// int main(){
+
+//     struct Stu s ={"zhangsan",12,"man","13032866683"};
+
+//     printf("%s %d %s %s\n",s.name,s.age,s.sex,s.tele);
+
+//     prt(&s);
 
 
-    return 0;
-}
+
+
+//     return 0;
+// }
---
+++ c/stu1/选择.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+int main(){
+    int day = 0;
+    scanf("%d",&day);
+
+
+    switch(day){
+        case 1:
+        printf("星期1");
+        case 2:
+        printf("星期2");
+        case 3:
+        printf("星期3");
+        case 4:
+        printf("星期4");
+        case 5:
+        printf("星期5");
+        case 6:
+        printf("星期6");
+        case 7:
+        printf("星期天");
+
+
+
+
+
+
+    }
+
+
+
+
+    return 0;
+}
