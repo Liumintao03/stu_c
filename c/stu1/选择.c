dd8617f04112ead5c6667b7fe41cf5fea9ebc5fe
liumintao2003 <3537693856@qq.com>
2025-08-13 20:15:48 +0800

    vault backup: 2025-08-13 20:15:47

--- c/stu1/选择.c
+++ c/stu1/选择.c
@@ -8,17 +8,17 @@ int main(){
 
     switch(day){
         case 1:
-        printf("星期1");
+        printf("星期1");break;
         case 2:
-        printf("星期2");
+        printf("星期2");break;
         case 3:
-        printf("星期3");
+        printf("星期3");break;
         case 4:
-        printf("星期4");
+        printf("星期4");break;
         case 5:
-        printf("星期5");
+        printf("星期5");break;
         case 6:
-        printf("星期6");
+        printf("星期6");break;
         case 7:
         printf("星期天");
 
