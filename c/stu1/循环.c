8e90ca01752ecb8be1b43615ff1b13ecee2bdfc7
liumintao2003 <3537693856@qq.com>
2025-08-24 11:28:58 +0800

    vault backup: 2025-08-24 11:28:58

--- c/stu1/循环.c
+++ c/stu1/循环.c
@@ -27,29 +27,68 @@
 // } 
 
 
+// int main(){
+
+//     int pwd[20]={0};
+
+//     printf("请输入密码");
+//     scanf("%s",pwd);
+//     //若要停止输入：Windows下按组合键Ctrl+Z，Unix/Linux/Mac下按组合键Ctrl+D。EOF就是终止的意思 
+//     // #include <iostream>
+//     // using namespace std;
+//     // int main() {
+//     //     int c;
+//     //     while((c = getchar()) != EOF){
+//     //         putchar(c);
+//     //     }
+//     //     return 0;
+//     // }
+//     getchar();
+//     printf("请确认密码");
+//     int ret = getchar();
+
+//     if('Y'==ret){
+//         printf("Yes\n");
+//     }
+//     else{
+//         printf("No\n");
+//     }
+
+
+
+
+
+
+//     return 0;
+// }
+
+
+
+
+
+//计算 1!+2!+3!+4!+......n!
+
 int main(){
 
-    int pwd[20]={0};
+    int i=1;
+    int j=1;
+    int n=0;
+    int ret =1;
+    int sum = 0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
 
-    printf("请输入密码");
-    scanf("%s",pwd);
+        for(j=1;j<=i;j++){
+            ret = ret*j;
+        }
+        sum = sum+ret;
+        ret = 1;
 
-    getchar();
-    printf("请确认密码");
-    int ret = getchar();
-
-    if('Y'==ret){
-        printf("Yes\n");
     }
-    else{
-        printf("No\n");
-    }
 
-
-
+    printf("%d\n",sum);
 
 
 
     return 0;
 }
-
