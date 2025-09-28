2ebd3cac8436b552f8bbab598fdfe4711df1abe7
liumintao2003 <3537693856@qq.com>
2025-09-28 09:12:47 +0800

    vault backup: 2025-09-28 09:12:46

--- c/stu1/practice/求100-200之间的素数.c
+++ c/stu1/practice/求100-200之间的素数.c
@@ -5,11 +5,13 @@
 int main(){
 
     for(int i=100;i<=200;i++){
-        for(int j=2;j<=200;j++){
+        for(int j=2;j<=i;j++){
+            printf("这个数是%d",i);
             if(i%j==0){
+                printf("这个数不是素数");
                 break;
             }
-            else if (j==200)
+            else if (j==i)
             {
                 printf("%d\n",i);
             }
