5566a444f12ce0a7ff02f3841811cc49293d8577
liumintao2003 <3537693856@qq.com>
2025-10-11 16:22:37 +0800

    vault backup: 2025-10-11 16:22:37

--- c/Vscode_stu_Sruct_c/struct02.c
+++ c/Vscode_stu_Sruct_c/struct02.c
@@ -1,32 +1,32 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-typedef struct ABC{
-    char a;
-    int b;
-    char c;
-    short d;
-    unsigned long long e;
-    char f;
-}ABC;
+// typedef struct ABC{
+//     char a;
+//     int b;
+//     char c;
+//     short d;
+//     unsigned long long e;
+//     char f;
+// }ABC;
 
-int main(){
+// int main(){
 
-    ABC abc;
+//     ABC abc;
 
-    memset(&abc,0,sizeof(abc));
-    abc.a=0x12;
-    abc.b=0x12345678;
-    abc.c=0x11;
-    abc.d=0x1122;
-    abc.e=0x1111111111111111;
-    abc.f=0x21;
-    printf("%lu\n",sizeof(struct ABC));
+//     memset(&abc,0,sizeof(abc));
+//     abc.a=0x12;
+//     abc.b=0x12345678;
+//     abc.c=0x11;
+//     abc.d=0x1122;
+//     abc.e=0x1111111111111111;
+//     abc.f=0x21;
+//     printf("%lu\n",sizeof(struct ABC));
 
-    char *p=&abc;
-    for(int i=0;i<sizeof(abc);i++){
-        printf("[0x%d]:%hx\n",i,p[i]);
-    }
+//     char *p=&abc;
+//     for(int i=0;i<sizeof(abc);i++){
+//         printf("[0x%d]:%hx\n",i,p[i]);
+//     }
 
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ c/Vscode_stu_Sruct_c/weiyu01.c
@@ -0,0 +1,21 @@
+//位域结构体
+
+#include <stdio.h>
+ struct MyStruct {
+ unsigned int a : 4;  // 5 bits
+ unsigned int b : 4;  // 3 bits
+ unsigned int c : 6;  // 6 bits
+ };
+ int main() {
+    struct MyStruct s = {15, 5, 45};
+
+    struct MyStruct abc;
+    memset(&abc,0,sizeof(abc));
+    printf("%d,%d\n",abc.a,abc.b);
+
+    abc.a=0b10010101;
+    
+    printf("%d\t%d",abc.a,abc.b);
+    //printf("a = %u, b = %u, c = %u\n", s.a, s.b, s.c);
+    return 0;
+ }
