42a72c2846904b316f51964ddb32666cf49839a4
liumintao2003 <3537693856@qq.com>
2025-10-11 15:21:45 +0800

    vault backup: 2025-10-11 15:21:45

--- c/Vscode_stu_Sruct_c/struct02.c
+++ c/Vscode_stu_Sruct_c/struct02.c
@@ -18,5 +18,11 @@ int main(){
     abc.d=0x1122;
     printf("%lu\n",sizeof(struct ABC));
 
+    char *p=&abc;
+    for(int i=0;i<sizeof(abc);i++){
+        printf("[0x%d]:%hx\n",i,p[i]);
+    }
+
+
     return 0;
 }
