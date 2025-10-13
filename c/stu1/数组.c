0b0cd1a6ca7e0d0cbabeeb8653a1950715d3fd90
liumintao2003 <3537693856@qq.com>
2025-10-13 19:25:37 +0800

    vault backup: 2025-10-13 19:25:37

--- c/stu1/数组.c
+++ c/stu1/数组.c
@@ -52,23 +52,69 @@
 // }
 
 
-#include<stdio.h>
+// #include<stdio.h>
 
 
 
-int main(){
+// int main(){
 
-    int num = 100;
-    int *p = &num;
-    printf("%d\n",p[0]);
+//     int num = 100;
+//     int *p = &num;
+//     printf("%d\n",p[0]);
 
 
-    return 0;
+//     return 0;
+// }
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+
+int main() {
+
+    int arr[5] = {2, 3, 4, 5, 6};
+    printf("arr:%x\t,arr:%p\n", arr,arr);
+    printf("&arr[0]:%x\t,&arr[0]:%p\n", &arr[0],&arr[0]);
+    
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%p: *(arr + %d): %d\t",arr + i,  i, *(arr + i));
+    }
+    puts("");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%p: arr[%d]: %d\t",&arr[i],  i, arr[i]);
+    }
+    puts("");
+    puts("");
+
+
+
+    int *p = arr;
+    printf("p: %p\n", p);
+
+        for (int i = 0; i < 5; i++)
+    {
+        printf("%p: *(p + %d): %d\t",p + i,  i, *(p + i));
+    }
+    puts("");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%p: p[%d]: %d\t",&p[i],  i, p[i]);
+    }
+    puts("");
+
+
+	return 0;
 }
-
-
-
-
 
 
 
