3026cff1ad872bcc799d5b271542c82a492bfb04
liumintao2003 <3537693856@qq.com>
2025-10-13 19:05:17 +0800

    vault backup: 2025-10-13 19:05:17

--- c/stu1/指针.c
+++ c/stu1/指针.c
@@ -18,25 +18,72 @@
 
 
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <time.h>
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <unistd.h>
+// #include <string.h>
+// #include <time.h>
 
 
-int main() {
+// int main() {
 
 
-    int num = 100;
-    int *p = &num;
-    int **pp = &p;
+//     int num = 100;
+//     int *p = &num;
+//     int **pp = &p;
 
-    printf("num:%d, *p:%d, **pp:%d\n", num, *p, **pp);
-    printf("&num:%p, p:%p, *pp:%p\n", &num, p, *pp);
-    printf("&p:%p, pp:%p\n", &p, pp);
-    printf("&pp:%p\n", &pp);
+//     printf("num:%d, *p:%d, **pp:%d\n", num, *p, **pp);
+//     printf("&num:%p, p:%p, *pp:%p\n", &num, p, *pp);
+//     printf("&p:%p, pp:%p\n", &p, pp);
+//     printf("&pp:%p\n", &pp);
 	
 
-	return 0;
-}
+// 	return 0;
+// }
+
+// #include <stdio.h>
+// #include <unistd.h>
+// #include <string.h>
+// #include <time.h>
+
+// struct abc{
+// 	char a;
+// 	int b;
+// 	short c;
+// };
+
+// int main() {
+
+// 	char *p1;
+// 	int *p2;
+// 	double *p3;
+// 	struct abc *p4;
+// 	int (*p5)(int, int);
+
+// 	printf("sizeof(char *): %lu\n", sizeof(char *));
+// 	printf("sizeof(int *): %lu\n", sizeof(int *));
+// 	printf("sizeof(double *): %lu\n", sizeof(double *));
+// 	printf("sizeof(struct abc*): %lu\n", sizeof(struct abc*));
+// 	printf("sizeof(int (*)(int,int)): %lu\n", sizeof(int (*)(int, int )));
+
+
+
+// 	return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
