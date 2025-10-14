300c4119e584543694eb7d01f704cc55cfd85058
liumintao2003 <3537693856@qq.com>
2025-10-14 19:51:10 +0800

    vault backup: 2025-10-14 19:51:09

--- c/guoxinan_stu/10_15/字符空间.c
+++ c/guoxinan_stu/10_15/字符空间.c
@@ -1,93 +1,93 @@
-#include<stdio.h>
-#include<string.h>
+// #include<stdio.h>
+// #include<string.h>
 
-int strcmp(const char *cs, const char *ct){
-    unsigned char c1, c2;
-    while (1) {
-        c1 = *cs++;
-        c2 = *ct++;
-        if (c1 != c2)
-            return c1 < c2 ? -1 : 1;
-        if (!c1)
-            break;
-    }
-    return 0;
-}
-int fun(int *p,int size){
-    for(int i=0;i<size;i++){
-        printf("%d\t",p[i]);
-    }
-    printf("\n");
-
-
-    return 0;
-}
-
-//  char *strncpy(char *dest, const char *src, size_t count)
-//  {
-//     char *tmp = dest;
-//     while (count) {
-//         if ((*tmp = *src) != 0)
-//             src++;
-//         tmp++;
-//         count--;
+// int strcmp(const char *cs, const char *ct){
+//     unsigned char c1, c2;
+//     while (1) {
+//         c1 = *cs++;
+//         c2 = *ct++;
+//         if (c1 != c2)
+//             return c1 < c2 ? -1 : 1;
+//         if (!c1)
+//             break;
 //     }
-//     return dest;
-//  }
+//     return 0;
+// }
+// int fun(int *p,int size){
+//     for(int i=0;i<size;i++){
+//         printf("%d\t",p[i]);
+//     }
+//     printf("\n");
 
-int main(){
+
+//     return 0;
+// }
+
+// //  char *strncpy(char *dest, const char *src, size_t count)
+// //  {
+// //     char *tmp = dest;
+// //     while (count) {
+// //         if ((*tmp = *src) != 0)
+// //             src++;
+// //         tmp++;
+// //         count--;
+// //     }
+// //     return dest;
+// //  }
+
+// int main(){
     
-    // char *str1="hello";
-    // char *str2="hello";
-    // char *str3="hello";
+//     // char *str1="hello";
+//     // char *str2="hello";
+//     // char *str3="hello";
 
-    // printf("\"hello\":%p\n","hello");
+//     // printf("\"hello\":%p\n","hello");
     
-    // printf("str1:%p\n",str1);
-    // printf("str2:%p\n",str2);
-    // printf("str3:%p\n",str3);
+//     // printf("str1:%p\n",str1);
+//     // printf("str2:%p\n",str2);
+//     // printf("str3:%p\n",str3);
 
-    // int arr[]={10,20,30,40,50};
-    // fun(arr,sizeof(arr)/sizeof(arr[0]));
+//     // int arr[]={10,20,30,40,50};
+//     // fun(arr,sizeof(arr)/sizeof(arr[0]));
 
-    // char arr1[]="12345678";
-    // char arr2[]={'1','2','3','a'};
-    // printf("%d\t,------%s\n",sizeof(arr2),arr2);
+//     // char arr1[]="12345678";
+//     // char arr2[]={'1','2','3','a'};
+//     // printf("%d\t,------%s\n",sizeof(arr2),arr2);
 
 
-    // char arr[]="rong";
-    // printf("%zd\n",sizeof(arr)/sizeof(arr[0]));
-    // printf("%zd\n",sizeof(arr));
+//     // char arr[]="rong";
+//     // printf("%zd\n",sizeof(arr)/sizeof(arr[0]));
+//     // printf("%zd\n",sizeof(arr));
 
-    // char str[]={'a','b','c','\0','0'};
-    // printf("%zd\n",sizeof(str)/sizeof(str[0]));
-    // printf("%zd\n",sizeof(str));
+//     // char str[]={'a','b','c','\0','0'};
+//     // printf("%zd\n",sizeof(str)/sizeof(str[0]));
+//     // printf("%zd\n",sizeof(str));
     
-    // printf("%s\n",arr);
-    // printf("%s\n",str);
+//     // printf("%s\n",arr);
+//     // printf("%s\n",str);
 
-    char buf[256]={0};
+//     char buf[256]={0};
 
-    strcpy(buf,"hello");
+//     strcpy(buf,"hello");
 
-    strcat(buf,"world");
+//     strcat(buf,"world");
 
-    printf("buf:%s\n",buf);
+//     printf("buf:%s\n",buf);
 
     
 
-    char *str ="123";
-    int num = 0;
-    int i= 0;
-    while(str[i] != 0){
-        num = num*10 + (str[i]-'0');
-        i++;
-    }
-    printf("%d\n",num);
+//     char *str ="123";
+//     int num = 0;
+//     int i= 0;
+//     while(str[i] != 0){
+//         num = num*10 + (str[i]-'0');
+//         i++;
+//     }
+//     printf("%d\n",num);
 
 
 
-    return 0;
+//     return 0;
 
 
-}
+// }
---
+++ c/homework/11.15/11.15_00.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+
+
+
+
+void test01(){
+    char buf1[] = "abc";
+    char buf2[] = {'a','b','c'};
+    printf("strlen_buf1:%d\tsizeof_buf1:%d\n",strlen(buf1),sizeof(buf1));//len=3，siziof=4是因为最后有\0
+    printf("strlen_buf2:%d\tsizeof_buf2:%d",strlen(buf2),sizeof(buf2));// len=3是因为strlen没有读取到\0就会一直读取，直到读取到\0为止
+
+}
+
+
+void test02(){
+    char buf1[7] = "abc";
+    char buf2[7] = {'a','b','c'};
+    printf("strlen_buf1:%d\tsizeof_buf1:%d\n",strlen(buf1),sizeof(buf1));//len=3，siziof=4是因为最后有\0
+    printf("strlen_buf2:%d\tsizeof_buf2:%d",strlen(buf2),sizeof(buf2));// len=3是因为strlen没有读取到\0就会一直读取，直到读取到\0为止
+
+}
+
+int main(){
+
+    test01();
+
+
+    return 0;
+}
