1cc2262b4773150234f8313cf99c588c863772ab
liumintao2003 <3537693856@qq.com>
2025-10-14 16:59:09 +0800

    vault backup: 2025-10-14 16:59:09

---
+++ c/guoxinan_stu/10_15/字符空间.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+
+int main(){
+    
+    char *str1="hello";
+    char *str2="hello";
+    char *str3="hello";
+
+    printf("\"hello\":%p\n","hello");
+    
+    printf("str1:%p\n",str1);
+    printf("str2:%p\n",str2);
+    printf("str3:%p\n",str3);
+
+
+    return 0;
+}
