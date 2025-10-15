294c4b7ea661598a36b374b8a377ab618d81d59e
liumintao2003 <3537693856@qq.com>
2025-10-15 18:51:00 +0800

    vault backup: 2025-10-15 18:50:59

--- c/homework/11.16/11.16_00.c
+++ c/homework/11.16/11.16_00.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 
 
+struct abc{
 
+};
 
 void test01(int *a,int *b){//交换两个变量
     printf("\n\ntest01\n");
@@ -14,7 +16,41 @@ void test01(int *a,int *b){//交换两个变量
 }
 
 void test02(){//定义指针变量，保存下⾯数组名。
-    char *p[6]={"data1","data2","data3","daata4","data5","data6"}; 
+    printf("\n\ntest02\n");
+    char data1[5];        
+    float data2[5];
+    struct abc data3[5];
+    char data4[4][5];
+    char *data5[5];
+    char **data6[3][4];
+    //char *p[6]={"data1","data2","data3","daata4","data5","data6"}; 
+
+    char (*p1)[5]=&data1;
+    float (*p2)[5]=&data2;
+    struct abc (*p3)[5]=&data3;
+    char (*p4)[5]=data4;
+    char (*p5)[5]=data5;
+    char (*p6)[4]=data6;
+
+
+
+
+
+
+
+
+
+}
+
+void test03(){
+    printf("\n\ntest03\n");
+
+    char *addr[] = {"hello","world", "good","bye"};
+
+    for(int i=0;i<sizeof(*addr)/sizeof(*addr[0]);i++){
+        printf("%s %s\n%s%s")
+    }
+
 
 }
 
