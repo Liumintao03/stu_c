45f7b08072d656fe8b2a4021970a02b594b6175f
liumintao2003 <3537693856@qq.com>
2025-10-15 18:30:41 +0800

    vault backup: 2025-10-15 18:30:41

--- c/homework/11.16/11.16_00.c
+++ c/homework/11.16/11.16_00.c
@@ -2,9 +2,23 @@
 
 
 
+
+void test01(int *a,int *b){//交换两个变量
+    printf("\n\ntest01\n");
+    printf("交换前a:%d,b:%d\n",*a,*b);
+    int *c=a;
+    a=b;
+    b=c;
+    printf("交换后a:%d,b:%d\n",*a,*b);
+
+}
+
+
 int main(){
 
-    test01();
+
+    int a=1,b=2;
+    test01(&a,&b);
 
 
 
