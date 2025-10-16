a688bfb75845aaff888ab37ccd543a43f93b2ac0
liumintao2003 <3537693856@qq.com>
2025-10-16 19:57:43 +0800

    vault backup: 2025-10-16 19:57:43

--- c/homework/11.16/myCalc.c
+++ c/homework/11.16/myCalc.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
+#include<string.h>
+
+int sum(char *s){
+    int num = 0;
+    int i=0;
+    while(*s=0){
+        num = num*10+(s[i]-'0');
+        s++;
+    }
+    return num;
+}
+
+
 int main(int argc, char *argv[]) {
     for(int i = 0; i < argc; ++i) {
         printf("argv[%d]:%s\n", i, argv[i]);
     }
+
+    int num1 = sum(argv[1]);
+    int num2 = sum(argv[2]);
 
 
     return 0;
