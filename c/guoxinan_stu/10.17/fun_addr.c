c2c68d563a671fb0618b8d7cf80f5f2e1aae7426
liumintao2003 <3537693856@qq.com>
2025-10-17 08:49:02 +0800

    vault backup: 2025-10-17 08:49:02

--- c/guoxinan_stu/10.17/fun_addr.c
+++ c/guoxinan_stu/10.17/fun_addr.c
@@ -9,6 +9,16 @@ int main(){
     printf("fun:0x%p\n",fun);
     printf("fun:0x%p\n",fun);
 
+    int arr[5];
+    int*p1 = arr;
+
+    int (*p2)(const char*,...)=printf;
+    printf("%p\n",printf);
+    printf("%p\n",p2);
+
+    p2("hello world\n");
+
+
 
     return 0;
 }
