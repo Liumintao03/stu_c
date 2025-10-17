93543d479ffc408048b90fdb31df718ef5f15823
liumintao2003 <3537693856@qq.com>
2025-10-17 15:45:56 +0800

    vault backup: 2025-10-17 15:45:56

--- c/guoxinan_stu/day08/fun_addr.c
+++ c/guoxinan_stu/day08/fun_addr.c
@@ -1,31 +1,63 @@
-#include<stdio.h>
-#include<string.h>
+// #include<stdio.h>
+// #include<string.h>
 
-void fun(){}
-void test01(){
-    printf("===test01===");
-    printf("fun:0x%p\n",fun);
-    printf("fun:0x%p\n",fun);
+// void fun1(){}
 
-    int arr[5];
-    int*p1 = arr;
+// void fun2(int a,int b){
+//     printf("fun:a=0x%d\n",a);
+//     printf("fun:b=0x%d\n",b);
 
-    int (*p2)(const char*,...)=printf;
-    printf("%p\n",printf);
-    printf("%p\n",p2);
+//     int num1;
+//     int num2;
+//     printf("fun:num1=0x%d\n",num1);
+//     printf("fun:num2=0x%d\n",num2);
 
-    p2("hello world\n");
-}
+// }
 
 
+// void test01(){
+//     printf("===test01===\n\n");
+//     printf("fun:0x%p\n",fun1);
+//     printf("fun:0x%p\n",fun1);
 
-int main(){
+//     int arr[5];
+//     int*p1 = arr;
 
-    test01();
+//     int (*p2)(const char*,...)=printf;
+//     printf("%p\n",printf);
+//     printf("%p\n",p2);
+
+//     p2("hello world\n");
+// }
+
+// void test02(){
+//     printf("\n===test02===\n\n");
+//     int a = 0x12345678;
+//     int b = 0xa1b2c3d4;
+
+//     printf("test02:&a=0x%d\n",&a);
+//     printf("test02:&b=0x%d\n",&b);
+
+//     fun2(a,b);
 
 
 
-    return 0;
-}
+
+
+// }
+
+// int main(){
+
+//     test01();
+//     test02();
+
+
+
+
+
+
+
+//     return 0;
+// }
 
 
