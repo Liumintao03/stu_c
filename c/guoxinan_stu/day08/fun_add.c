cf999282277dd7d9419a50684292eab852884aa9
liumintao2003 <3537693856@qq.com>
2025-10-17 16:06:12 +0800

    vault backup: 2025-10-17 16:06:12

--- c/guoxinan_stu/day08/fun_add.c
+++ c/guoxinan_stu/day08/fun_add.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
-int func(int num1,int num2){
-    int num = num1 + num2;
-    return num;
+// #include<stdio.h>
+// int func(int num1,int num2){
+//     int num = num1 + num2;
+//     return num;
 
-}
+// }
 
 
 
-int main(int argcc,char *argv[]){
-    int num1 = 100;
-    int num2 = 200;
-    int num = func(num1,num2);
+// int main(int argcc,char *argv[]){
+//     int num1 = 100;
+//     int num2 = 200;
+//     int num = func(num1,num2);
 
-    printf("%d\n",num);
-    return 0;
-}
+//     printf("%d\n",num);
+//     return 0;
+// }
