6f641e1209e522f316d67cfac7710d8412d8444c
liumintao2003 <3537693856@qq.com>
2025-10-17 09:09:20 +0800

    vault backup: 2025-10-17 09:09:20

--- c/homework/11.16/myCalc.c
+++ c/homework/11.16/myCalc.c
@@ -1,29 +1,29 @@
-#include<stdio.h>
-#include<string.h>
+// #include<stdio.h>
+// #include<string.h>
 
-int sum(char *s){
-    int num = 0;
-    int i=0;
-    //printf("%s\n",s);
-    while(*s!=0){
-        num = num*10+(s[i]-'0');
-        //printf("%d\n",num);
-        s++;
-    }
-    //printf("%d\n",num);
+// int sum(char *s){
+//     int num = 0;
+//     int i=0;
+//     //printf("%s\n",s);
+//     while(*s!=0){
+//         num = num*10+(s[i]-'0');
+//         //printf("%d\n",num);
+//         s++;
+//     }
+//     //printf("%d\n",num);
 
-    return num;
-}
+//     return num;
+// }
 
 
-int main(int argc, char *argv[]) {
-    // for(int i = 0; i < argc; ++i) {
-    //     printf("argv[%d]:%s\n", i, argv[i]);
-    // }
+// int main(int argc, char *argv[]) {
+//     // for(int i = 0; i < argc; ++i) {
+//     //     printf("argv[%d]:%s\n", i, argv[i]);
+//     // }
 
-    int num1 = sum(argv[1]);
-    int num2 = sum(argv[3]);
+//     int num1 = sum(argv[1]);
+//     int num2 = sum(argv[3]);
 
-    printf("%d\n",num1+num2);
-    return 0;
- }
+//     printf("%d\n",num1+num2);
+//     return 0;
+//  }
