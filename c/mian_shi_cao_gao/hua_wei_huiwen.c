3e5edc09a1cf28488c336d6bb53e64c5eee6df4b
liumintao2003 <3537693856@qq.com>
2025-10-18 16:18:53 +0800

    vault backup: 2025-10-18 16:18:53

--- c/mian_shi_cao_gao/hua_wei_huiwen.c
+++ c/mian_shi_cao_gao/hua_wei_huiwen.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 #include<string.h>
 int IFhuiwen(char *str){
-    char temp[strlen(str)];
+    char temp[strlen(str)+1];
     for(int i=0;i<strlen(str);i++){
         temp[i] = str[strlen(str)-1-i];
         printf("%c\t%c\n",temp[i],str[i]);
     }
     printf("%s\t%s\n",temp,str);
-
-    while(temp!=0){
-        if(temp!=str){
+    temp[strlen(str)] = '\0';
+    int i=0;
+    while(str[i]!=0){
+        if(temp[i]!=str[i]){
+            printf("%c\t%c\n",temp[i],str[i]);
+            
             return -1;
         }
-        temp++;
+        temp[i++];
         str++;
     }
     return 0;
