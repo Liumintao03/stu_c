ff0fefa8048014a2f47f2a00fa4a6a25f3ecd18b
liumintao2003 <3537693856@qq.com>
2025-10-18 15:58:33 +0800

    vault backup: 2025-10-18 15:58:33

--- c/mian_shi_cao_gao/hua_wei_strcmp.c
+++ c/mian_shi_cao_gao/hua_wei_strcmp.c
@@ -2,13 +2,14 @@
 
 int strcmp(char *arr1,char *arr2){
 
-    while(*arr1 || *arr2  ){
-        if(*arr1 != *arr2){
-            return -1;
-        }
+    while(*arr1 && *arr2 && *arr1 == *arr2){
         arr1++;
         arr2++;
-
+    }
+    if(*arr1==*arr2){
+        return 0;
+    }else{
+        return -1;
     }
 
     return 0;
@@ -16,8 +17,8 @@ int strcmp(char *arr1,char *arr2){
 
 int main(){
 
-    char arr1 = "asdf";
-    char arr2 = "adsf";
+    char arr1[] = "asdf";
+    char arr2[] = "adsf";
 
     printf("%d",strcmp(arr1,arr2));
 
