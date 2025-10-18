399a62dcd09b0a350ab773261dfaa28cd419d65e
liumintao2003 <3537693856@qq.com>
2025-10-18 16:29:04 +0800

    vault backup: 2025-10-18 16:29:03

--- c/mian_shi_cao_gao/hua_wei_huiwen.c
+++ c/mian_shi_cao_gao/hua_wei_huiwen.c
@@ -1,31 +1,40 @@
 #include<stdio.h>
 #include<string.h>
-int IFhuiwen(char *str){
+int IFhuiwen(char *str){//复制出一个相反的字符串，再进行比较
     char temp[strlen(str)+1];
     for(int i=0;i<strlen(str);i++){
         temp[i] = str[strlen(str)-1-i];
-        printf("%c\t%c\n",temp[i],str[i]);
     }
-    printf("%s\t%s\n",temp,str);
     temp[strlen(str)] = '\0';
     int i=0;
-    while(str[i]!=0){
+    for(int i=0;i<strlen(str);i++){
         if(temp[i]!=str[i]){
-            printf("%c\t%c\n",temp[i],str[i]);
-            
             return -1;
         }
-        temp[i++];
-        str++;
     }
     return 0;
 }
 
+int huiwen(char *str){
+    for(int i=0;i<strlen(str)/2;i++){
+        if(str[i]!=str[strlen(str)-1-i]){
+            return -1;
+        }
+            
+    }
+    return 0;
+}
+
+
+
 int main(){
 
     char str[] = "123321";
-    int RET = IFhuiwen(str);
-    printf("%d\n",RET);
+    int RET1 = IFhuiwen(str);
+    int RET2 = huiwen(str);
+    printf("%d\n",RET1);
+    printf("%d\n",RET2);
+
 
 
     return 0;
