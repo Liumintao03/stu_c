3c6301bcd70d6f672c91e6f698234425ff252098
liumintao2003 <3537693856@qq.com>
2025-10-18 15:28:03 +0800

    vault backup: 2025-10-18 15:28:03

---
+++ c/mian_shi_cao_gao/hua_wei_02.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void strcmp(char *arr1,char *arr2){
+
+    while(((*arr1++==*arr2++)!="\0") ){
+
+    }
+
+
+}
+
+int main(){
+
+    char arr1 = {"asdf"};
+    char arr2 = {"adsf"};
+
+
+    return 0;
+}
