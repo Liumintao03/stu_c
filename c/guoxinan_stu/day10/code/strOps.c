e9f2cbf865c2fd4759af7fd129fef8daf8c669e1
liumintao2003 <3537693856@qq.com>
2025-10-21 16:58:14 +0800

    vault backup: 2025-10-21 16:58:14

--- c/guoxinan_stu/day10/code/strOps.c
+++ c/guoxinan_stu/day10/code/strOps.c
@@ -37,3 +37,29 @@ int delestr(char *str,char ch,int len){//删除字符空间⾥指定的字符，
     return 0;
 
 }
+
+int checkstr(char *str,char *tar){//查找⼀个⻓串中，短串出现的位置，
+    if(str == NULL || tar == NULL){
+        return -1;
+    }
+    int i = 0;
+    while(str[i] != '\0'){
+        int j=0;
+        if(str[i] == tar[0]){
+            while(tar[j] != '\0'){
+                if(str[i+j] == tar[j]){
+                    j++;
+                }
+                else if(tar[j] == '\0'){
+                    return i+1;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        i++;
+    }
+    return -1;
+}
+
