426ab0022ecdb284456fbb5779f3607ae0e96f6c
liumintao2003 <3537693856@qq.com>
2025-10-21 16:37:54 +0800

    vault backup: 2025-10-21 16:37:54

--- c/guoxinan_stu/day10/code/strOps.c
+++ c/guoxinan_stu/day10/code/strOps.c
@@ -19,16 +19,16 @@ int strnum(char *p,char ch){//统计字符串中指定字符出现的个数
 }
 
 
-int delestr(char **str,char ch,int len){//删除字符空间⾥指定的字符，必须是可变字符串
+int delestr(char *str,char ch,int len){//删除字符空间⾥指定的字符，必须是可变字符串
     if(str==NULL){
         return -1;
     }
     int i=0;
-    while((*str)[i]!='\0'){
-        char *tem = *str+1;
+    while(str[i]!='\0'){
+        char *tem = str+1;
 
-        if((*str)[i]==ch){
-            strcat((*str)[i],tem[i]);
+        if(str[i]==ch){
+            strcat(str+i,tem);
         }
         i++;
     }
