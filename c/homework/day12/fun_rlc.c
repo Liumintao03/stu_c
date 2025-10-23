836ac6c81efe3bd66f1ed027a6d07df2fc40fd4c
liumintao2003 <3537693856@qq.com>
2025-10-23 18:18:11 +0800

    vault backup: 2025-10-23 18:18:11

--- c/homework/day12/fun_rlc.c
+++ c/homework/day12/fun_rlc.c
@@ -9,6 +9,8 @@ void *encode_rlc(void *data,int *len){
     //2==malloc申请堆空间,用来存放编码后的数据
     //3==从源空间开始,不断的找到数据,以及其对应连续的个数,如果连续个数超过0xff,则 从新计数
     //4==把数据以及对应的个数存入到编码空间
+    printf("开始编码1\n");
+
     if(data == NULL){
         printf("传入的需要编码为空");
         return NULL;
@@ -25,6 +27,8 @@ void *encode_rlc(void *data,int *len){
         }
 
     }
+    printf("开始编码2\n");
+
     sybm = 0;//重置标记
     cunt = cunt * 2;//因为每个空间都要有其需要计数的数字,所以要×2
     if(cunt>255){
@@ -33,6 +37,8 @@ void *encode_rlc(void *data,int *len){
     }
     char *endata = (char *)malloc(cunt);//申请空间
     int addrs = 0;
+    printf("开始编码3\n");
+
     for(int i = 0;i < src_len; i++){//编码过程
         if(src_data[sybm] != src_data[i]){//如果遇到改变的节点
             addrs+=2;  //编码的空间更新
@@ -46,6 +52,8 @@ void *encode_rlc(void *data,int *len){
             endata[addrs+1] = 1;
         }
     }
+    printf("endata[]:%xsn",endata);
+
     *len = cunt;
     return endata;
 }
@@ -122,16 +130,16 @@ int main(){
     unsigned char data[] = {0x11, 0x11, 0x11,0x22, 0x22, 0x22, 0x33,0x45, 0x56, 0x56, 0x56};
     int len = sizeof(data)/sizeof(data[0]);
     show_rlcinfo(data,&len);
-
+    printf("初始化结束\n\n\n");
     //编码
     unsigned char *encode_data = (unsigned char *)encode_rlc(data,&len);
     show_rlcinfo(data,&len);
     int incode_len = len;
-    printf("len:%d\n",len);
-
+    printf("编码结束len:%d\n\n\n",len);
     //解码
     unsigned char *decode_data = (unsigned char *)decode_rlc(data,&len);
     show_rlcinfo(data,&len);
+    printf("解码结束len:%d\n\n\n",len);
     
 
     free_rlc(encode_data);
