2601fbfb485112b44522cf4b7849c8ebd1d9a414
liumintao2003 <3537693856@qq.com>
2025-10-30 22:38:24 +0800

    vault backup: 2025-10-30 22:38:24

--- c/Clion_stu_c/struct_data/Heap/Heap.h
+++ c/Clion_stu_c/struct_data/Heap/Heap.h
@@ -5,4 +5,22 @@
 #ifndef CLION_STU_C_HEAP_H
 #define CLION_STU_C_HEAP_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef int Element;
+#define MAXSIZE 200
+
+typedef struct Heap{
+    Element data[MAXSIZE];
+    int front;//头
+    int rear;//尾
+    int count;
+}Heap;
+//创建初始化队列
+Heap *createInitHeap();
+//
+
+
 #endif //CLION_STU_C_HEAP_H
