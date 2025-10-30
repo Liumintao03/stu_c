1fd81333bc8c66dc06ed917ff6a2fcbf160233b1
liumintao2003 <3537693856@qq.com>
2025-10-30 23:39:31 +0800

    vault backup: 2025-10-30 23:39:31

--- c/Clion_stu_c/struct_data/Heap/Heap.c
+++ c/Clion_stu_c/struct_data/Heap/Heap.c
@@ -8,12 +8,31 @@
 Heap *createInitHeap(){
     Heap *heap = (Heap *)malloc(sizeof (Heap));
     memset(heap,0,sizeof (Heap));
-    
+    heap->count=0;
+    heap->front = 1;
 }
 //插入
-int insertHeap(Heap *heap,Element *e);
+int insertHeap(Heap *heap,Element *e){
+    //判断合法性
+    if(heap == NULL){
+        printf("传入的为空\n");
+        return -1;
+    }
+    if(heap->front==(heap->rear+1)%MAXSIZE){
+        printf("栈满了\n");
+        return -1;
+    }
+    //插入元素,后面那位更新
+    heap->data[(heap->rear+1)%MAXSIZE]==*e;
+    heap->rear=(heap->rear+1%MAXSIZE);
+
+
+}
 //删除
-int deletHeap(Heap *heap);
+int deletHeap(Heap *heap){
+    //校验合法性
+    
+}
 //打印
 int printHeapinfo(Heap *heap);
 //释放
