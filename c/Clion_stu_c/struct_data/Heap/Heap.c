fa9f19c6ddfabd22f01be88b845cd1354eddc81c
liumintao2003 <3537693856@qq.com>
2025-10-30 23:49:42 +0800

    vault backup: 2025-10-30 23:49:42

--- c/Clion_stu_c/struct_data/Heap/Heap.c
+++ c/Clion_stu_c/struct_data/Heap/Heap.c
@@ -9,7 +9,8 @@ Heap *createInitHeap(){
     Heap *heap = (Heap *)malloc(sizeof (Heap));
     memset(heap,0,sizeof (Heap));
     heap->count=0;
-    heap->front = 1;
+    heap->front =0;
+    heap->rear = 0;
 }
 //插入
 int insertHeap(Heap *heap,Element *e){
@@ -19,24 +20,58 @@ int insertHeap(Heap *heap,Element *e){
         return -1;
     }
     if(heap->front==(heap->rear+1)%MAXSIZE){
-        printf("栈满了\n");
+        printf("队列满了,无法添加\n");
         return -1;
     }
     //插入元素,后面那位更新
     heap->data[(heap->rear+1)%MAXSIZE]==*e;
     heap->rear=(heap->rear+1%MAXSIZE);
-
+    return 0;
 
 }
 //删除
 int deletHeap(Heap *heap){
     //校验合法性
-    
+    if(heap == NULL){
+        printf("传入的为空\n");
+        return -1;
+    }
+    if(heap->rear==heap->front){
+        printf("队列为空,无法删除\n");
+        return -1;
+    }
+    //删除
+    heap->front = (heap->front+1)%MAXSIZE;
+    return 0;
+
 }
 //打印
-int printHeapinfo(Heap *heap);
+int printHeapinfo(Heap *heap){
+    if(heap == NULL){
+        printf("传入的为空\n");
+        return -1;
+    }
+    if(heap->rear==heap->front){
+        printf("队列为空,没有元素打印\n");
+        return -1;
+    }
+    for(int i = heap->rear;i!=heap->front;i++){
+        printf("%d\t",heap->data[i]);
+        i = (i+1)&MAXSIZE;
+    }
+    printf("\n");
+    return 0;
+}
 //释放
-int releaseHeap(Heap *heap);
+int releaseHeap(Heap *heap){
+    if(heap == NULL){
+        printf("传入的为空\n");
+        return -1;
+    }
+    free(heap);
+    printf("释放完成\n");
+    return 0;
+}
 
 
 
--- c/Clion_stu_c/struct_data/Heap/main.c
+++ c/Clion_stu_c/struct_data/Heap/main.c
@@ -3,3 +3,25 @@
 //
 #include "Heap.h"
 
+int main(){
+
+    //创建队列
+    Heap *heap = createInitHeap();
+    //添加
+    for(int i = 0;i<=10;i++){
+        insertHeap(heap,&i);
+    }
+    //打印
+    printHeapinfo(heap);
+    //删除
+    for(int i = 0;i<=10;i++){
+        deletHeap(heap);
+    }
+    //打印
+    printHeapinfo(heap);
+    //释放
+    releaseHeap(heap);
+
+
+    return 0;
+}
