f211bbca37927e3b46df3feeec92df75a3c95b87
liumintao2003 <3537693856@qq.com>
2025-10-31 16:13:13 +0800

    vault backup: 2025-10-31 16:13:13

--- c/Clion_stu_c/struct_data_func/List_opposite/list_opposite.h
+++ c/Clion_stu_c/struct_data_func/List_opposite/list_opposite.h
@@ -5,4 +5,24 @@
 #ifndef CLION_STU_C_LIST_OPPOSITE_H
 #define CLION_STU_C_LIST_OPPOSITE_H
 
+typedef int Element;
+
+typedef struct Listnode{
+    Element data;
+    struct Listnode *next;
+}Listnode;
+
+//创建链表
+Listnode *createListnode(Listnode *listnode);
+//插入尾插
+int addelementListnode(Listnode *listnode,Element *e);
+//释放
+int releaseListnode(Listnode *listnode,Element *e);
+//翻转
+int oppositeListnode(Listnode *listnode);
+//合并
+int 
+
+
+
 #endif //CLION_STU_C_LIST_OPPOSITE_H
