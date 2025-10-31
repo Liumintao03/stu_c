b80b471073ab7930ca2f25488b8769c4750c70c1
liumintao2003 <3537693856@qq.com>
2025-10-31 09:16:39 +0800

    vault backup: 2025-10-31 09:16:39

--- c/Clion_stu_c/struct_data/ListHeap/ListHeap.c
+++ c/Clion_stu_c/struct_data/ListHeap/ListHeap.c
@@ -21,12 +21,18 @@ HeapList *createListHeapInit(){
 int addListHeap(HeapList *heapList,Element *e){
     //判断合法性
     if(heapList==NULL){
-        printf("队列不存在");
+        printf("添加队列不存在\n");
         return -1;
     }
     Heap *tmp = (Heap *) malloc(sizeof (Heap));
     tmp->data = *e;
     tmp->next = NULL;
+    if(heapList->font==NULL &&heapList->rear==NULL){//开始时候创建
+        heapList->font = tmp;
+        heapList->rear = tmp;
+        return 0;
+    }
+
     heapList->rear->next = tmp;
     heapList->rear = tmp;
     heapList->count++;
@@ -35,25 +41,58 @@ int addListHeap(HeapList *heapList,Element *e){
 //尾出队
 int popListHeap(HeapList *heapList){
     if(heapList==NULL){
-        printf("队列不存在");
+        printf("删除队列不存在\n");
         return -1;
     }
 
-    if(heapList->rear = heapList->font){
-        printf("队列为空");
+    if(heapList->font==NULL||heapList->rear== NULL){
+        printf("删除队列为空\n");
         return -1;
     }
-    Heap *tmp = heapList->font->next;
-    heapList->font
-
-
-
+    Heap *tmp = heapList->font;
+    heapList->font==heapList->font->next;
+    free(tmp);
+    if(heapList->font == NULL){
+        heapList->rear == NULL;
+    }
+    heapList->count--;
+    return 0;
 }
 
 //打印
-int printHeapListinfo(HeapList *heapList);
+int printHeapListinfo(HeapList *heapList){
+    if(heapList==NULL){
+        printf("打印队列不存在\n");
+        return -1;
+    }
+
+    if(heapList->font==NULL||heapList->rear== NULL){
+        printf("打印队列为空\n");
+        return -1;
+    }
+    Heap *tmp = heapList->font;
+    for(int i = heapList->count;i>=1;i++){
+        printf("%d\t",tmp->data);
+        tmp = tmp->next;
+    }
+    printf("打印完成\n");
+    return 0;
+
+
+}
 //释放
-int releaseHeapList(HeapList *heapList);
+int releaseHeapList(HeapList *heapList){
+    if(heapList==NULL){
+        printf("释放队列不存在\n");
+        return -1;
+    }
+
+    if(heapList->font==NULL||heapList->rear== NULL){
+        printf("释放队列为空\n");
+        return -1;
+    }
+    
+}
 
 
 
