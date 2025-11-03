24b0cab15d3f5f280a92a4d5c9a09ca800725b96
liumintao2003 <3537693856@qq.com>
2025-11-03 16:35:39 +0800

    vault backup: 2025-11-03 16:35:39

--- c/Clion_stu_c/struct_data/Hash/Hash.h
+++ c/Clion_stu_c/struct_data/Hash/Hash.h
@@ -4,5 +4,44 @@
 
 #ifndef CLION_STU_C_HASH_H
 #define CLION_STU_C_HASH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define Element int
+
+//定义节点
+typedef struct HashNode{
+    //键值
+    int key;
+    //存放的值
+    Element value;
+    //下一个节点
+    struct  HashNode *next;
+}HashNode;
+
+//定义哈希表
+typedef struct HashTable{
+    //大小
+    int size;
+    //表的指针数组,头指针就是hashtable
+    struct HashTable **hashtable;
+
+}HashTable;
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
 
 #endif //CLION_STU_C_HASH_H
