0949c2a5703a6f0671f8c6e82956f15b17f0f75e
liumintao2003 <3537693856@qq.com>
2025-11-03 17:06:12 +0800

    vault backup: 2025-11-03 17:06:12

--- c/Clion_stu_c/struct_data/Hash/Hash.c
+++ c/Clion_stu_c/struct_data/Hash/Hash.c
@@ -12,7 +12,7 @@ HashTable *createinithasbtable(int size){
     HashTable *hashTable = (HashTable *) malloc(sizeof (HashTable));
     //判断是否申请成功
     if(hashTable==NULL){
-        printf("申请哈希表失败");
+        printf("申请哈希表失败\n");
         return NULL;
     }
     //确认表大小
@@ -25,11 +25,38 @@ HashTable *createinithasbtable(int size){
 
 //计算位置函数
 static int funcHash(int key,int size){
-    
+    return key%size;
 }
 
 //2.插入元素,传入表,插入元素的键值,元素的值
-int insertHashElement(HashTable *hashTable,int key,Element e);
+int insertHashElement(HashTable *hashTable,int key,Element e){
+    //验证元素合法性
+    if(hashTable == NULL){
+        printf("要插入元素的哈希表为空\n");
+        return -1;
+    }
+    //插入
+    //找到插入节点
+    int index = funcHash(key,hashTable->size);
+    //创建新节点
+    HashNode *newNode = (HashNode *)malloc(sizeof (HashNode));
+    newNode->key = key;
+    newNode->value = e;
+    newNode->next = NULL;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 //3.删除元素
 int deletHashElement(HashTable *hashTable,int key);
