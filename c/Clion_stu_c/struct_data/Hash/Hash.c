c3ed5f2995f7fa9817171beed3d55be14fe13ccf
liumintao2003 <3537693856@qq.com>
2025-11-03 18:27:27 +0800

    vault backup: 2025-11-03 18:27:27

--- c/Clion_stu_c/struct_data/Hash/Hash.c
+++ c/Clion_stu_c/struct_data/Hash/Hash.c
@@ -45,28 +45,50 @@ int insertHashElement(HashTable *hashTable,int key,Element e){
     newNode->next = NULL;
     //如果这个地方没有节点,直接插入
     if(hashTable->table[index]==NULL){
-        hashTable->table[index]
+        //这个指针数组的这一个元素存放新的节点
+        hashTable->table[index] = newNode;
+        return 0;
     }
-
-    //判断是否有相同key节点,有的话直接改变值并且释放新建节点
-
-
-
-
-
-
-
-
-
-
-
-
-
+    else{//说明这个指针存放了,如果要插入得插在这个后面,或者改变后面相同key的值
+        //开始时标记头结点,随后使用这个tmp指针来进行操作
+        HashNode *tmp = hashTable->table[index];
+        while(tmp!=NULL)//以这个节点来判断链表是否还存在节点
+        {
+            //判断是否有相同key节点,有的话直接改变值并且释放新建节点
+            if(tmp->key == key){
+                tmp->value = e;
+                return 0;
+            }
+            //在里面判断tmp是否走到最后一个有效节点,在外面判断的话,无法找到前一个节点,导致无法插入
+            if(tmp->next == NULL){
+                break;
+            }
+            //刷新节点
+            tmp = tmp->next;
+        }
+        //此时tmp指向的就是最后一个有效节点,直接可以插入到tmp后面
+        tmp->next = newNode;
+        return 0;
+    }
 
 }
 
 //3.删除元素
-int deletHashElement(HashTable *hashTable,int key);
+int deletHashElement(HashTable *hashTable,int key){
+    //判断元素合法性
+    if(hashTable==NULL){
+        printf("要删除元素的哈希表不存在\n");
+        return -1;
+    }
+    //记录是在表的第几行的链表
+    int index = funcHash(key,hashTable->size);
+    //定下链表头的节点,用来进行对链表的操作
+    HashNode *tmp = hashTable->table[index];
+    //进行这个哈希表的对应行的链表遍历,直到找到要删除的节点
+    while(tmp!=NULL){
+        if()
+    }
+}
 
 //4.查找
 int searchHash(HashTable *hashTable,int key);
