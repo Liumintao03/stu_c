30598f52d87bade0a54a387115d947cda597694c
liumintao2003 <3537693856@qq.com>
2025-11-03 22:11:29 +0800

    vault backup: 2025-11-03 22:11:28

--- c/Clion_stu_c/C_homework/arr_half_select/arr_half_select.c
+++ c/Clion_stu_c/C_homework/arr_half_select/arr_half_select.c
@@ -1,3 +1,45 @@
 //
 // Created by 35376 on 2025/11/3.
 //
+
+
+
+//数组二分查找
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int arr_half_select(int *arr,int len,int e){
+    int addr = len/2;
+    int min = 0;
+    int max = len-1;
+    while(arr[addr]!=e){
+        if(arr[addr]>e){
+            max = addr;
+            addr = (min+max)/2;
+        }else if(arr[addr]<e){
+            min = addr;
+            addr = (min+max)/2;
+        }
+    }
+    return addr;
+}
+
+
+
+
+int main(){
+    int arr[50];
+    for(int i = 0;i<50;i++){
+        arr[i] = i;
+    }
+    int len = sizeof (arr)/sizeof (arr[0]);
+    int res = arr_half_select(arr,len,13);
+    printf("%d",res);
+
+
+
+
+}
