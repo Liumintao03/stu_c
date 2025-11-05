c50e8183d1711aa0f0de69dcc3be3bb176c6f676
liumintao2003 <3537693856@qq.com>
2025-11-05 15:55:01 +0800

    vault backup: 2025-11-05 15:55:01

---
+++ c/Ubuntu_c_code/Ubuntu_home/cmake-build-debug-home_linux/CMakeFiles/Ubuntu_home.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ubuntu_home.dir/main.c.o"
+  "Ubuntu_home"
+  "Ubuntu_home.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ubuntu_home.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
