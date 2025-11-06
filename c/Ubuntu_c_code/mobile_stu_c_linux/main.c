9a6401429188224222a14934340d2474cdd98854
liumintao2003 <3537693856@qq.com>
2025-11-06 15:59:53 +0800

    vault backup: 2025-11-06 15:59:52

--- c/Ubuntu_c_code/mobile_stu_c_linux/main.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/main.c
@@ -8,7 +8,7 @@ int main(void) {
     printf("Hello, World!\n");
 
 
-    int fd = open("a.txt",O_RDONLY);
+    int fd = open("a.txt",O_RDWR);
     if(fd == -1){
         perror("open");
         return -1;
@@ -20,12 +20,14 @@ int main(void) {
     //ssize_t read(int fd, void *buf, size_t count);
     //write()
     //ssize_t write(int fd, const void *buf, size_t count);
+    //指针偏移
+    //off_t lseek(int fd, off_t offset, int whence);
     int ret = write(fd,"abc",3);
     if(ret<0){
         perror("write:");
         return -1;
     }
-
+    printf("write success,size = %d\n",ret);
 
     close(fd);
     return 0;
