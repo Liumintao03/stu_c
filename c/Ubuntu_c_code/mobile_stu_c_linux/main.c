81f3ffef040c93822be0a0c3d1992554092c6f05
liumintao2003 <3537693856@qq.com>
2025-11-06 11:55:50 +0800

    vault backup: 2025-11-06 11:55:50

--- c/Ubuntu_c_code/mobile_stu_c_linux/main.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/main.c
@@ -2,12 +2,19 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(void) {
     printf("Hello, World!\n");
 
-    
-    int fd = open("a.txt",int flags);
 
+    int fd = open("a.txt",O_RDWR);
+    if(fd == -1){
+        perror("open");
+        return -1;
+    }
+    printf("open success\n");
+
+    close(fd);
     return 0;
 }
