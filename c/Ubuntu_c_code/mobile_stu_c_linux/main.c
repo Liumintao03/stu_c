3744a3c99ab1ff1083fe72eba81cd6574097dcd3
liumintao2003 <3537693856@qq.com>
2025-11-06 15:28:59 +0800

    vault backup: 2025-11-06 15:28:58

--- c/Ubuntu_c_code/mobile_stu_c_linux/main.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/main.c
@@ -17,8 +17,11 @@ int main(void) {
 
 
     //read()
-    ssize_t read(int fd, void *buf, size_t count);
+    //ssize_t read(int fd, void *buf, size_t count);
     //write()
+    //ssize_t write(int fd, const void *buf, size_t count);
+
+
 
     close(fd);
     return 0;
