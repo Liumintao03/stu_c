dc8296fb284353221ff91837c34c76f50bc72bc7
liumintao2003 <3537693856@qq.com>
2025-11-06 16:53:32 +0800

    vault backup: 2025-11-06 16:53:32

--- c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01sysio/01open.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01sysio/01open.c
@@ -8,14 +8,23 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int mian(){
+int main(){
     //open 打开一个文件
-    //不存在的时候，创建一个文件
-    int fb = open("abc.ini",O_CREAT,0664);
+    //不存在的时候,文件名,创建一个文件|赋予fb操作权限,赋予文件操作权限
+    int fb = open("abc.ini",O_CREAT|O_RDWR,0664);
+    if(fb<0){
+        perror("open:");
+        return -1;
+    }
+    printf("=========\n");
+
+    int ret = write(fb,"hello\n",6);
+    printf("ret:%d\n",ret);
+
     
 
 
-
+    close(fb);
     return 0;
 }
 
