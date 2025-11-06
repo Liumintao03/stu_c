00e5c60debcdc8b7fc0d0ce23dfe55eec59cd243
liumintao2003 <3537693856@qq.com>
2025-11-06 18:45:14 +0800

    vault backup: 2025-11-06 18:45:14

--- c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01sysio/01open.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01sysio/01open.c
@@ -24,7 +24,7 @@ int main(){
     //lseek指针偏移，返回偏移位数
     //off_t lseek(int fd, off_t offset, int whence);
     int ret_num = lseek(fb,0,SEEK_END);
-    printf("file_size%d\n",ret_num);
+    printf("file_size: %d\n",ret_num);
 
     //使指针回到最开头
     lseek(fb,0,SEEK_SET);
