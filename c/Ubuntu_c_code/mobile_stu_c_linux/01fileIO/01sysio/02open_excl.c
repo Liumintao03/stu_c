02bd4eac3d6ee0df7788448ff37f95979fc2237b
liumintao2003 <3537693856@qq.com>
2025-11-06 18:55:33 +0800

    vault backup: 2025-11-06 18:55:33

---
+++ c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01sysio/02open_excl.c
@@ -0,0 +1,32 @@
+//
+// Created by 35376 on 2025/11/6.
+//
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int mian(){
+
+    //excl
+    //文件不存在新建,存在忽略
+    //O_CREAT|O_RDWR
+
+    //文件存在报错
+    //O_CREAT|O_EXCL|O_RDWR
+
+    //文件不存在新建
+    //O_
+
+    int fb = open("abc.ini",O_CREAT|O_EXCL|O_RDWR,0644);
+
+
+
+    return 0;
+}
+
+
