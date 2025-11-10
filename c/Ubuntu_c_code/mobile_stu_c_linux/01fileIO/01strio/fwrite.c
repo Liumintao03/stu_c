5d0f5bdf077973156749d9fa2d44adec61054934
liumintao2003 <3537693856@qq.com>
2025-11-10 19:35:43 +0800

    vault backup: 2025-11-10 19:35:43

--- c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/fwrite.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/fwrite.c
@@ -16,12 +16,8 @@ int main() {
         perror("fopen");
         return -1;
     }
-
     //创建成功
     int ret;
-
-
-
     //写入
     //=========用哪里的内容====一个元素大小===写多少个元素===写到那里去
     ret = fwrite(arr,sizeof (arr[0]),3,fp);
@@ -31,13 +27,25 @@ int main() {
         return -1;
     }
     printf("\nret = %d\n",ret);
-
-
+    //重值指针
+    fseek(fp,0,SEEK_SET);
     //读取
     fp = fopen("new_test","r");
-    
-
-
+    //判断是否创建指针成功
+    if(fp==NULL){
+        perror("fopen:");
+        return -1;
+    }
+    //创建成功
+    ret = fread(arr,sizeof (arr[0]),3,fp);
+    if(ret<0){
+        perror(" fread:");
+        return -1;
+    }
+    printf("\nret:%d\n",ret);
+    for(int i = 0;i<sizeof (arr)/sizeof (arr[0]);i++){
+        printf("%lf\n",arr[i]);
+    }
 
 
     //清理
