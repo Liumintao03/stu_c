81c851c1b0e5ca7ab88cb04f6ad085513f27d90e
liumintao2003 <3537693856@qq.com>
2025-11-10 19:15:20 +0800

    vault backup: 2025-11-10 19:15:20

---
+++ c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/fwrite.c
@@ -0,0 +1,19 @@
+//
+// Created by 35376 on 2025/11/10.
+//
+
+
+//块池存储
+#include <stdio.h>
+
+
+
+int main(){
+    char 
+
+
+
+
+    return 0;
+}
+
---
+++ c/Ubuntu_c_code/mobile_stu_c_linux/build_dir/01fileIO/01strio/CMakeFiles/01fwrite.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01fwrite"
+  "01fwrite.pdb"
+  "CMakeFiles/01fwrite.dir/fwrite.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/01fwrite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
