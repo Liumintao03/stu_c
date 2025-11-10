8ea9e9da70d27ef0f598c2558f6b3d2e010f971f
liumintao2003 <3537693856@qq.com>
2025-11-10 22:25:34 +0800

    vault backup: 2025-11-10 22:25:34

--- c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/homwork.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/homwork.c
@@ -5,15 +5,17 @@
 #include <stdio.h>
 
 int fun_fgetc(int *argc,char *argv,FILE *fp);
-int fun_fgets(int *argc,char *argv,FILE *fp);
-
+int fun_read(int *argc,char *argv,FILE *fp);
 
 int main(int argc,char *argv[]){
-    
+
     char *old_file = "/home/lmt/project/C_lion/stu_linux_c/build_dir/01fileIO/src_file";
     //1.选择是按fgetc/fputc 还是 按fread/ fwrite【块的⼤⼩按1B】的程序
     // 定下操作指针
     FILE *fp = fopen(old_file,"r");
+    int ret;
+
+
     //判断打开是否有效
     if(fp==NULL){
         perror("open_r:\n");
@@ -21,9 +23,16 @@ int main(int argc,char *argv[]){
     }
     //打开成功，判断fgetc还是fread
     if(argv[1]=="char"||argv[1]=="block"){
-
+        //进入fgetc操作
+        if(argv[1]=="char"){
+            ret = fun_fgetc(&argc,argv,fp);
+        }
+        //进入fread操作
+        else if(argv[1]=="block"){
+            ret = fun_read(&argc,argv,fp);
+        }
     }else{
-        printf("操作错误")
+        printf("操作错误");
         return -1;
     }
 
@@ -33,10 +42,11 @@ int main(int argc,char *argv[]){
 
 //实现fgetc
 int fun_fgetc(int *argc,char *argv,FILE *fp){
+    
 
 }
 
 //实现fgetc
-int fun_fgets(int *argc,char *argv,FILE *fp){
+int fun_read(int *argc,char *argv,FILE *fp){
 
 }
