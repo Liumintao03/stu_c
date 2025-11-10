eeec3ef8d029105e2d760cc5fd5b15d95400e1f7
liumintao2003 <3537693856@qq.com>
2025-11-10 17:03:21 +0800

    vault backup: 2025-11-10 17:03:21

--- c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/fgetc_putchar.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/fgetc_putchar.c
@@ -4,6 +4,19 @@
 
 
 int mian(){
+    char *path = "/home/lmt/project/C_lion/stu_linux_c/build_dir/src.file";
+    FILE *ret;
+    //判断要打开的文件是否存在
+    ret = fopen(path,"-r");
+    //如果不存在
+    if(ret==NULL){
+        perror("fopen");
+        return -1;
+    }
+
+    //如果存在
+    int cnt = 0;
+    
 
 
 
