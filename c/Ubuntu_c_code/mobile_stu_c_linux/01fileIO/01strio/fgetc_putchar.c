3f751fac03ad496a209f5c95eb2b161785f45ea3
liumintao2003 <3537693856@qq.com>
2025-11-10 17:13:35 +0800

    vault backup: 2025-11-10 17:13:34

--- c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/fgetc_putchar.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/fgetc_putchar.c
@@ -1,22 +1,37 @@
-
+//一个一个字节打印
 //标准io，为c支持的io,所以调用的是c库
 #include <stdio.h>
+#include <unistd.h>
 
 
-int mian(){
+int main(){
     char *path = "/home/lmt/project/C_lion/stu_linux_c/build_dir/src.file";
-    FILE *ret;
+    FILE *fp;
     //判断要打开的文件是否存在
-    ret = fopen(path,"-r");
+    fp = fopen(path,"r");
     //如果不存在
-    if(ret==NULL){
+    if(fp==NULL){
         perror("fopen");
         return -1;
     }
 
     //如果存在
+    //标记打印字节数量
     int cnt = 0;
-    
+    //将ret
+    int ret = fgetc(fp);
+    //直到ret走到最后
+    while(ret!=EOF){
+        sleep(1);
+        putchar(ret);
+
+        fflush(stdout);
+        ret = fgetc(fp);
+        cnt++;
+    }
+    printf("%d\n",cnt);
+    fclose(fp);
+
 
 
 
