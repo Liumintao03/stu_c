43c9c4e8f778e166a526db5cec6512b086a65fa6
liumintao2003 <3537693856@qq.com>
2025-11-10 19:25:32 +0800

    vault backup: 2025-11-10 19:25:32

--- c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/fwrite.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/01fileIO/01strio/fwrite.c
@@ -7,12 +7,41 @@
 #include <stdio.h>
 
 
+int main() {
+    double arr[] = {1.0, 3.14, 5.34, 6.22, 11.34};
+    FILE *fp = fopen("new_test","w+");
+    //判断是否创建指针成功
+    if(fp==NULL){
+        //输出错误信息
+        perror("fopen");
+        return -1;
+    }
 
-int main(){
-    char 
+    //创建成功
+    int ret;
 
 
 
+    //写入
+    //=========用哪里的内容====一个元素大小===写多少个元素===写到那里去
+    ret = fwrite(arr,sizeof (arr[0]),3,fp);
+    //如果没有写入成功
+    if(ret<0){
+        perror("fwrite");
+        return -1;
+    }
+    printf("\nret = %d\n",ret);
+
+
+    //读取
+    fp = fopen("new_test","r");
+    
+
+
+
+
+    //清理
+    fclose(fp);
 
     return 0;
 }
