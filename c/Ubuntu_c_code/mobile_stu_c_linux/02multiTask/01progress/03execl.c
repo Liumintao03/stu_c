d5bfcb1510317fde56f42695827323c9dd9e2d6e
liumintao2003 <3537693856@qq.com>
2025-11-12 23:09:36 +0800

    vault backup: 2025-11-12 23:09:35

--- c/Ubuntu_c_code/mobile_stu_c_linux/02multiTask/01progress/03execl.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/02multiTask/01progress/03execl.c
@@ -1,3 +1,20 @@
 //
 // Created by 35376 on 2025/11/12.
 //
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+
+    printf("before execl\n");
+    int ret;
+    ret = execlp("ls","随便填","-a","-l",NULL);
+    if(ret ==-1){
+        fprintf(stderr,"execl error");
+        printf("error\n");
+    }
+    printf("after execl\n");
+
+
+    return 0;
+}
