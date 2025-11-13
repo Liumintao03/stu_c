add947494e29d44d738682cc83b9b7ce92fe9cfc
liumintao2003 <3537693856@qq.com>
2025-11-13 18:49:25 +0800

    vault backup: 2025-11-13 18:49:25

--- c/Ubuntu_c_code/mobile_stu_c_linux/02multiTask/02pthread/01pthread_self.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/02multiTask/02pthread/01pthread_self.c
@@ -1,3 +1,17 @@
 //
 // Created by 35376 on 2025/11/13.
 //
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+
+    printf("pthread_self\t%d\n", pthread_self());
+
+    printf("getpid\t%d\n",getpid());
+
+
+    return 0;
+}
