41b052dd874b4b94612189946072576126d83fdc
liumintao2003 <3537693856@qq.com>
2025-11-18 19:00:02 +0800

    vault backup: 2025-11-18 19:00:02

--- c/Ubuntu_c_code/mobile_stu_c_linux/03ipc/01pipe/homework1_1.c
+++ c/Ubuntu_c_code/mobile_stu_c_linux/03ipc/01pipe/homework1_1.c
@@ -6,14 +6,54 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
 
 
-int main(){
+pthread_mutex_t mutex;
+
+int main() {
+    //初始化锁
+    pthread_mutex_init(&mutex, NULL);
     //先创建管道，使得子进程和父进程都能使用
     int pipe_fd[2];
     int ret = pipe(pipe_fd);
-    
+    //检测管道创建是否成功
+    if (ret != 0) {
+        perror("pipe");
+        return -1;
+    }
+    //创建三个子进程
+    int child01 = fork();
+    int child02 = fork();
+    int child03 = fork();
+    if (child01 == 0) {
+        //锁上
+        pthread_mutex_lock(&mutex);
+        //第一个子进程
+        //定下buf
+        char buf[128];
 
+
+        pthread_mutex_unlock(&mutex);
+    } else if (child02 == 0) {
+        //锁上
+        pthread_mutex_lock(&mutex);
+        //第二个子进程
+
+
+
+        //解锁
+        pthread_mutex_unlock(&mutex);
+    } else if (child03 == 0) {
+        //锁上
+        pthread_mutex_lock(&mutex);
+        //第三个子进程
+
+
+        
+        //解锁
+        pthread_mutex_unlock(&mutex);
+    }
 
     return 0;
 }
